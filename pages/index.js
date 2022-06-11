bdd8fbe37d570390ee47c51c1dc5b5b65532005f
junbro <khm11904@gmail.com>
2022-06-11 13:52:45 +0900

    ✨ start 기능 추가

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Preview2 <a href="https://nextjs.org">Next.js!</a>
+          started <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
